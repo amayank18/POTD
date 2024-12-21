3c9b17a1376cd7e8342cb6be46a222a9c118fd17
amayank18 <amayank071@gmail.com>
2024-12-21 23:19:02 +0530

    Day1

---
+++ DAY1/stringScore.java
@@ -0,0 +1,18 @@
+package DAY1;
+
+import java.util.*;
+public class stringScore {
+    public static int scoreOfString(String s) {
+        int score=0;
+        for(int i=0;i<s.length()-1;i++) 
+            if(i!=s.length())
+                score+=Math.abs(s.charAt(i)-s.charAt(i+1));
+        return score;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int score=scoreOfString(s);
+        System.out.println(score);
+    }
+}
