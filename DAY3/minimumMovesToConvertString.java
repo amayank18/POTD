bde618670d9044bf08d40c3d5a15fa50f33a6b1a
amayank18 <amayank071@gmail.com>
2024-12-23 13:15:46 +0530

    Day3

---
+++ DAY3/minimumMovesToConvertString.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int count=0;
+        int n=s.length();
+        for(int i=0;i<n;i++) {
+            if(s.charAt(i)!='O') {
+                count++;
+                i+=2;
+            }
+        }
+        return count;
+    }
+}
