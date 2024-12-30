a39b9e6298383e1a35ccca7d9c4d782c3b5b2239
amayank18 <amayank071@gmail.com>
2024-12-30 16:23:29 +0530

    Day10

--- DAY10/deciBinary.java
+++ DAY10/deciBinary.java
@@ -1,4 +1,21 @@
 package DAY10;
 
 public class deciBinary {
+    public static int minPartitions(String s) {
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < s.length(); i++) {
+            int temp = s.charAt(i) - '0';
+            if (temp > ans) {
+                ans = temp;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        //String str="32";
+        //String str="82734";
+        String str="27346209830709182346";
+        System.out.println(minPartitions(str));
+    }
 }
