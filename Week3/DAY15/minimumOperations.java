0fd546897e5f53cb12389b195b9d634036de0dd1
amayank18 <amayank071@gmail.com>
2025-01-04 22:48:41 +0530

    Day15

--- Week3/DAY15/minimumOperations.java
+++ Week3/DAY15/minimumOperations.java
@@ -1,4 +1,43 @@
 package Week3.DAY15;
 
+import java.util.Arrays;
+
 public class minimumOperations {
+    //brute force
+//    public static int[] minOperations(String boxes) {
+//        int[] result=new int[boxes.length()];
+//        for(int i=0;i<boxes.length();i++) {
+//            for(int j=0;j<boxes.length();j++) {
+//                if(i!=j && boxes.charAt(j) == '1')
+//                    result[i]+=Math.abs(i-j);
+//            }
+//        }
+//        return result;
+//    }
+
+    //
+    public static int[] minOperations(String boxes) {
+        int[] result=new int[boxes.length()];
+        int travel = 0, ones = 0;
+        for(int i=0;i<boxes.length();i++) {
+            travel+=ones;
+            result[i]+=travel;
+            if(boxes.charAt(i)=='1')
+                ones++;
+        }
+        travel=0;
+        ones=0;
+        for(int i=boxes.length()-1;i>=0;i--) {
+            travel+=ones;
+            result[i]+=travel;
+            if(boxes.charAt(i)=='1')
+                ones++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String boxes="110";
+        System.out.print(Arrays.toString(minOperations(boxes)));
+    }
 }
--- Week3/DAY15/removeColoredPieces.java
+++ Week3/DAY15/removeColoredPieces.java
@@ -1,4 +1,21 @@
 package Week3.DAY15;
 
 public class removeColoredPieces {
+    public static boolean winnerOfGame(String colors) {
+        int alice=0,bob=0;
+        for(int i=1;i<colors.length()-1;i++) {
+            if(colors.charAt(i-1)==colors.charAt(i) && colors.charAt(i)==colors.charAt(i+1)) {
+                if(colors.charAt(i)=='A')
+                    alice++;
+                else
+                    bob++;
+            }
+        }
+        return alice>bob;
+    }
+
+    public static void main(String[] args) {
+        String colors = "AAABABB";
+        System.out.print(winnerOfGame(colors));
+    }
 }
